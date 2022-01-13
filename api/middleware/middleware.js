01cb48fb16e3b931c797d1384a39d831d6a9699c
abc <abc@example.com>
2022-01-12 21:36:20 -0700

    Can't figure out the last one

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -42,20 +42,17 @@ async function validateUser(req, res, next) {
   }
   }
 
-async function validatePost(req, res, next) {
+function validatePost(req, res, next) {
   // DO YOUR MAGIC
-  try {
-    const postText = await req.body.postText
-    if(!postText) {
-      res.status(400).json({
-        message: "missing required text field"
-      })
-    } else {
-      next()
-    }
-  } catch (err) {
-    res.status(500).json({message: "invalid"})
-}
+  const { text } = req.body
+  if (!text || !text.trim()) {
+    res.status(400).json({
+      message: "missing required text field"
+    })
+  } else {
+    req.text = text.trim()
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
